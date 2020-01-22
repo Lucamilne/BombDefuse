c7704bbee2eb668618282b86fabc548a26024112
Lucamilne <43890713+Lucamilne@users.noreply.github.com>
2020-01-22 18:24:51 +0000

    Update bombDefuse.js

--- bombDefuse.js
+++ bombDefuse.js
@@ -206,7 +206,7 @@ function backspaceButton() {
 var id;
 
 function blinkingText() {
-    display.textContent = display.textContent == "press" ? "red" : "press";
+    display.textContent = display.textContent == "press" ? "play" : "press";
 }
 
 function stopBlinkingText() {
